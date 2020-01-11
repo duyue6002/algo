ec9719ac2e31840b7f4dc3d6011742be1112a020
duyue6002 <duyue6002@gmail.com>
2020-01-11 20:22:49 +0800

    卖股票with colddown+DP状态转移方程

--- Week 05/leetcode_309.js
+++ Week 05/leetcode_309.js
@@ -30,4 +30,21 @@ var maxProfit = function(prices) {
   return sell;
 };
 
+/**
+ *
+ * @param {number[]} prices
+ * @returns {number}
+ */
+var maxProfit = function(prices) {
+  let dp = Array.from(Array(prices.length + 2), () => Array(2).fill(0));
+  dp[0][0] = dp[1][0] = 0; // 当前不持有股票，下一步可以买入，利润为0
+  dp[0][1] = dp[1][1] = Number.MIN_SAFE_INTEGER; // 开始时不可能持有股票，改情况不可能
+  for (i = 2; i <= prices.length + 1; i++) {
+    let price = prices[i - 2];
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + price);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - price);
+  }
+  return dp[dp.length - 1][0];
+};
+
 maxProfit([1, 2, 3, 0, 2]);
