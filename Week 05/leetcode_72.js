558c0c636a72e2db542b610037c2ee0912967a3f
duyue6002 <duyue6002@gmail.com>
2020-02-03 01:27:13 +0800

    编辑距离

---
+++ Week 05/leetcode_72.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  let len1 = word1.length,
+    len2 = word2.length;
+  let dp = Array.from(Array(len1 + 1), () => Array(len2 + 1).fill(0));
+  for (let i = 1; i <= len1; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 1; j <= len2; j++) {
+    dp[0][j] = j;
+  }
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (word1[i - 1] == word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+      }
+    }
+  }
+  return dp[len1][len2];
+};
+
+minDistance("horse", "ros");
