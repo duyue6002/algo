a1fc92ed8250f69df0ebf31469a51faab5c7459a
duyue6002 <duyue6002@gmail.com>
2020-02-12 18:05:34 +0800

    构建Trie

--- Week 06/leetcode_208.js
+++ Week 06/leetcode_208.js
@@ -1,26 +1,56 @@
-class Trie {
-  constructor() {
-    this.root = {};
-  }
+/**
+ * Initialize your data structure here.
+ */
+var Trie = function() {
+  this.root = {};
+};
 
-  insert(word) {
-    let node = this.root;
-    for (let i = 0; i < word.length; i++) {
-      node = node[word[i]] = node[word[i]] || {};
-      if (i === word.length - 1) node.isTerminal = true;
-    }
+/**
+ * Inserts a word into the trie.
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+  let cur = this.root;
+  for (let ch of word) {
+    cur[ch] = cur[ch] || {};
+    cur = cur[ch];
   }
+  cur.isWord = true;
+};
 
-  search(word, isPrefix) {
-    let node = this.root;
-    for (let c of word) {
-      if (!node[c]) return false;
-      node = node[c];
-    }
-    return isPrefix || !!node.isTerminal;
+Trie.prototype.traverse = function(word) {
+  let cur = this.root;
+  for (let ch of word) {
+    if (!cur) return null;
+    cur = cur[ch];
   }
+  return cur;
+};
 
-  startsWith(prefix) {
-    return this.search(prefix, true);
-  }
-}
+/**
+ * Returns if the word is in the trie.
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+  let node = this.traverse(word);
+  return !!node && !!node.isWord;
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix.
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+  return !!this.traverse(prefix);
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
