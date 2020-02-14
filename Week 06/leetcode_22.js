295ee6213453be7a217207b6cc0f03b95eaed098
duyue6002 <duyue6002@gmail.com>
2020-02-14 21:49:55 +0800

    括号生成问题

---
+++ Week 06/leetcode_22.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  let result = Array();
+  helper(result, "", n, 0, 0);
+  return result;
+};
+
+var helper = function(result, str, n, left, right) {
+  if (left === n && right === n) {
+    result.push(str);
+    return;
+  }
+
+  if (left < n) {
+    helper(result, str + "(", n, left + 1, right);
+  }
+
+  if (right < n && right < left) {
+    helper(result, str + ")", n, left, right + 1);
+  }
+};
