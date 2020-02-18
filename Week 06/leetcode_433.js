4aaf45dcf4631c956d4267910a6477ca744352c6
duyue6002 <duyue6002@gmail.com>
2020-02-18 20:22:47 +0800

    最小基因变化 单向BFS

--- Week 06/leetcode_433.js
+++ Week 06/leetcode_433.js
@@ -1,4 +1,5 @@
 /**
+ * 双向 BFS
  * @param {string} start
  * @param {string} end
  * @param {string[]} bank
@@ -28,6 +29,40 @@ var minMutation = function(start, end, bank) {
           }
           if (dict.has(tmp)) {
             beginQueue.push(tmp);
+            dict.delete(tmp);
+          }
+        }
+      }
+    }
+    ladder++;
+  }
+  return -1;
+};
+
+/**
+ * 单向 BFS
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+var minMutation = function(start, end, bank) {
+  if (!bank.includes(end)) return -1;
+  let queue = [];
+  queue.push(start);
+  let ladder = 0;
+  let dict = new Set(bank);
+
+  while (queue.length !== 0) {
+    let length = queue.length;
+    for (let i = 0; i < length; i++) {
+      let word = queue.shift();
+      if (word === end) return ladder;
+      for (let j = 0; j < word.length; j++) {
+        for (let ch of ["A", "G", "T", "C"]) {
+          let tmp = word.slice(0, j) + ch + word.slice(j + 1);
+          if (dict.has(tmp)) {
+            queue.push(tmp);
             dict.delete(tmp);
           }
         }
