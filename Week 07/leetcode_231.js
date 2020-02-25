8844bedb964c31d5632e2187b0b776f99e9548e1
duyue6002 <duyue6002@gmail.com>
2020-02-25 16:30:01 +0800

    2的幂

--- Week 07/leetcode_231.js
+++ Week 07/leetcode_231.js
@@ -3,8 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-  let helper = n & (n - 1);
-  return n > 0 && helper === 0;
+  return n > 0 && (n & (n - 1)) === 0;
 };
-
-isPowerOfTwo(2);
