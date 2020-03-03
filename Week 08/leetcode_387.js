3e5d62fb53ce42f20ef2964dc6c23fe8a9e6910a
duyue6002 <duyue6002@gmail.com>
2020-03-03 12:00:59 +0800

    字符串中的第一个唯一字符(map)

---
+++ Week 08/leetcode_387.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  let arr = [];
+  let counter = [];
+  for (let i = 0; i < s.length; i++) {
+    let index = arr.indexOf(s[i]);
+    if (index === -1) {
+      arr.push(s[i]);
+    } else {
+      counter.push(index);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (!counter.includes(i)) return i;
+  }
+  return -1;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  let map = {};
+  let first = s.length;
+  for (let i = 0; i < s.length; i++) {
+    let ch = s[i];
+    if (map[ch]) {
+      map[ch]["counter"]++;
+    } else {
+      map[ch] = { index: i, counter: 1 };
+    }
+  }
+  for (let ch of Object.keys(map)) {
+    if (map[ch]["counter"] === 1 && map[ch]["index"] < first) {
+      first = map[ch]["index"];
+    }
+  }
+  return first === s.length ? -1 : first;
+};
+
+firstUniqChar("leetcode");
