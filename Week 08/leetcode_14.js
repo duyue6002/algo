ba34103bb068434d6e9a56820112a9affcb84de1
duyue6002 <duyue6002@gmail.com>
2020-03-04 18:02:17 +0800

    最长公共前缀（横向扫描）

---
+++ Week 08/leetcode_14.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (!strs || strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, prefix.length - 1);
+    }
+  }
+  return prefix;
+};
+
+longestCommonPrefix(["aa", "ba"]);
