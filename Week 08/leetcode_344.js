b0240e38e3141cf08ede14f912a8ee7d4752cd36
duyue6002 <duyue6002@gmail.com>
2020-03-05 13:16:43 +0800

    反转字符串

---
+++ Week 08/leetcode_344.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  let end = s.length - 1;
+  let mid = s.length >> 1;
+  for (let i = 0; i < mid; i++) {
+    [s[i], s[end - i]] = [s[end - i], s[i]];
+  }
+};
+
+reverseString(["h", "e", "l", "l", "o"]);
