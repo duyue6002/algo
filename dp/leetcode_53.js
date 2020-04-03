f15bae9c75689706d7a30eec8214240002110486
duyue6002 <duyue6002@gmail.com>
2020-04-03 15:55:52 +0800

    maximum subarray

--- dp/leetcode_53.js
+++ dp/leetcode_53.js
@@ -9,3 +9,18 @@ var maxSubArray = function(nums) {
   }
   return Math.max(...arr);
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let dp = Array(nums.length);
+  dp[0] = nums[0];
+  let result = dp[0]; // 增加一个临时变量存储最大值
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    result = Math.max(result, dp[i]);
+  }
+  return result;
+};
