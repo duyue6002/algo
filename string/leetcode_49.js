eb8f9d5e313b00e4efe58df4a0ed6f166f2eb703
duyue6002 <duyue6002@gmail.com>
2020-04-07 14:02:41 +0800

    group anagrams

--- string/leetcode_49.js
+++ string/leetcode_49.js
@@ -2,14 +2,11 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
   let map = {},
     res = [];
   for (let str of strs) {
-    let sorted = str
-      .split("")
-      .sort()
-      .join("");
+    let sorted = str.split("").sort().join("");
     if (map.hasOwnProperty(sorted)) {
       map[sorted].push(str);
     } else {
@@ -20,4 +17,22 @@ var groupAnagrams = function(strs) {
     res.push(map[key]);
   }
   return res;
+};
+
+/**
+ * 用MAP数据结构加速
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  let map = new Map();
+  for (let str of strs) {
+    let sorted = str.split("").sort().join("");
+    if (map.has(sorted)) {
+      map.get(sorted).push(str);
+    } else {
+      map.set(sorted, [str]);
+    }
+  }
+  return [...map.values()];
 };
