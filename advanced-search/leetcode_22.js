41e0c97f0a725d79f259a5a8d18529d67ee30ef5
duyue6002 <duyue6002@gmail.com>
2020-04-09 16:45:57 +0800

    dp 括号生成

--- advanced-search/leetcode_22.js
+++ advanced-search/leetcode_22.js
@@ -23,26 +23,4 @@ var helper = function(result, str, n, left, right) {
   }
 };
 
-// DP 解法
-
-/**
- * @param {number} n
- * @return {string[]}
- */
-var generateParenthesis = function(n) {
-  let dp = Array(n + 1);
-  dp[0] = [""];
-  for (let i = 1; i <= n; i++) {
-    dp[i] = [];
-    for (let j = 0; j < i; j++) {
-      for (let k1 of dp[j]) {
-        for (let k2 of dp[i - 1 - j]) {
-          dp[i].push("(" + k1 + ")" + k2);
-        }
-      }
-    }
-  }
-  return dp[n];
-};
-
 generateParenthesis(3);
