963dd10d6defc4b5175c2ca22eba0ead7c7634b0
duyue6002 <duyue6002@gmail.com>
2020-05-03 22:06:47 +0800

    ransom note

---
+++ string/leetcode_383.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  let map = new Map();
+  for (let c of magazine) {
+    if (map.has(c)) {
+      map.set(c, map.get(c) + 1);
+    } else {
+      map.set(c, 1);
+    }
+  }
+  for (let c of ransomNote) {
+    if (!map.has(c) || map.get(c) <= 0) return false;
+    map.set(c, map.get(c) - 1);
+  }
+  return true;
+};
